f100a1506b909ee94567a4e42ac34806f1e94857
Cesar Mejia <cesar@prompt.com>
2021-07-01 10:02:28 -0500

    Configure store and declare associate types

---
+++ src/store.ts
@@ -0,0 +1,12 @@
+import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux'
+import { configureStore } from '@reduxjs/toolkit'
+
+export const store = configureStore({})
+
+// Typing our global app state and dispatch
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch // What we know as ReduxDispatch
+
+// Export typed versions of useDispatch (enables response to be inferred) and useSelector (enables state to be inferred)
+export const useAppDispatch = () => useDispatch<AppDispatch>() // What we know as useReduxDispatch
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
