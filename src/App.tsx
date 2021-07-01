f4c28bf838124094c823e7b78fff8bd31372496c
Cesar Mejia <cesar@prompt.com>
2021-07-01 10:53:39 -0500

    Add a select component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,26 @@
 import React, { useState } from 'react'
 import { Pokemon } from './components/Pokemon'
 
+const options = ['pikachu', 'psyduck', 'charizard', 'squirtle']
 export const App = () => {
-  return <Pokemon name="pikachu" />
+  const [selectedPokemon, setSelectedPokemon] = useState<string>()
+
+  return (
+    <div>
+      <label htmlFor="pokemon-select">Choose a pokemon:</label>
+      <select
+        name="pokemon"
+        id="pokemon-select"
+        value={selectedPokemon}
+        onChange={e => setSelectedPokemon(e.target.value)}
+      >
+        {options.map(name => (
+          <option key={name} value={name}>
+            {name}
+          </option>
+        ))}
+      </select>
+      {selectedPokemon && <Pokemon name={selectedPokemon} />}
+    </div>
+  )
 }
